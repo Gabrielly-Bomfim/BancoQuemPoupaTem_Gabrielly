24c6a546a98df54156cb9dd0e4bd8405e14f5ccc
Gabrielly-Bomfim <144944047+Gabrielly-Bomfim@users.noreply.github.com>
2024-05-16 23:33:55 -0300

    cadastrar

--- funcoes.h
+++ funcoes.h
@@ -1,16 +1,20 @@
-//esse struct vai ter as informações do cliente 
+
 #define CLIENTES 1000
 #define OPERACOES 100
-struct cliente {//definindo as variaveis 
+typedef struct {//definindo as variaveis que vamo pedir na função
     char nome[50];
     char cpf[15];
-    char tipoConta[20];
-    double valorInicial;
+    char tipo_conta[20];
+    double saldo;
     char senha[10];
-}; //definindo o struct
+    int num_operacoes;
+} CLIENTE;//definindo o struct
+
 
 //esse struct vai ter as informações das operações realizadas 
-struct OPERA {//definindo as variaveis 
-    char operacao_feita[50];
-    double valor;
-};
+//struct OPERA {//definindo as variaveis 
+  //  char operacao_feita[50];
+   // double valor;
+//};
+
+void criando_contatos(CLIENTE[], int*);
