5d5c3493a875223a4db9ffd4a229e35ed253a0ae
Gabrielly-Bomfim <144944047+Gabrielly-Bomfim@users.noreply.github.com>
2024-05-17 19:38:34 +0000

    apagar

--- func.c
+++ func.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "funcoes.h"
-
+//funções.c
 void salvar_cliente(CLIENTE CLIENTE_LIMITE[], int *num_clientes) {
     FILE *arquivo = fopen("cadastros.bin", "wb");
     if (arquivo == NULL) {
@@ -11,6 +11,9 @@ void salvar_cliente(CLIENTE CLIENTE_LIMITE[], int *num_clientes) {
     fwrite(CLIENTE_LIMITE, sizeof(CLIENTE), *num_clientes, arquivo);
     fclose(arquivo);
 }
+
+
+
 void carregar_clientes(CLIENTE CLIENTE_LIMITE[], int *num_clientes) {
     FILE *arquivo = fopen("clientes.bin", "rb");
     if (arquivo == NULL) {
@@ -21,6 +24,44 @@ void carregar_clientes(CLIENTE CLIENTE_LIMITE[], int *num_clientes) {
     fread(CLIENTE_LIMITE, sizeof(CLIENTE), num_clientes, arquivo);
     fclose(arquivo);
 }
+
+
+void apaga_cliente(CLIENTE CLIENTE_LIMITE[], int *num_clientes){
+   
+    //apagar clientes
+      if(*num_clientes == 0){
+          printf("-------------Não há contatos salvos-------------\n");
+          return;
+        }
+      
+      int cpf;
+      printf("Digite o cpf do cliente: ");
+      scanf("%d", &cpf);
+      
+      limpar_buffer();
+      if(cpf<*num_clientes || cpf <0){
+        printf("ERRO\n");
+        return;
+    }
+
+      for (int n = cpf; n < *num_clientes - 1; n++) {
+          strcpy(CLIENTE_LIMITE[n].nome, CLIENTE_LIMITE[n+1].nome);
+          strcpy(CLIENTE_LIMITE[n].cpf, CLIENTE_LIMITE[n+1].cpf);
+          strcpy(CLIENTE_LIMITE[n].tipo_conta, CLIENTE_LIMITE[n+1].tipo_conta);
+          CLIENTE_LIMITE[n].saldo = CLIENTE_LIMITE[n+1].saldo;
+          strcpy(CLIENTE_LIMITE[n].senha, CLIENTE_LIMITE[n+1].senha);
+          CLIENTE_LIMITE[n].num_operacoes = CLIENTE_LIMITE[n+1].num_operacoes;
+      }
+    
+      (*num_clientes)--;
+      *num_clientes = *num_clientes - 1;
+    FILE *arquivo = fopen("cadastros.bin", "wb");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;}
+    fclose(arquivo);
+}
+  
 
 void listar_clientes(CLIENTE CLIENTE_LIMITE[], int *num_clientes) {
     printf("\nLista de Clientes:\n");
